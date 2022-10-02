9d5ff2f462d001ec7597547c1432542dab2e7f32
tongari123 <108406294+tongari123@users.noreply.github.com>
2022-10-02 19:28:17 +0900

    Revert "ユーザー管理機能の実装"
    This reverts commit 5d1808cf40bac6ba2986814b15125e21b3473d8b.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,24 +1,4 @@
 class ItemsController < ApplicationController
   def index
   end
-
-  def new
-    @item = Item.new
-
-  end
-
-
-  def create
-    @item = Item.new(item_params)
-
-  end
-
-
-
-
-
 end
-
-
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :items
 
   root to: 'items#index'
-  resources :items 
-
 
 
 end
