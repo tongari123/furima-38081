12eace6e76d4e394b3bb8aebc6c595aaaed1e900
tongari123 <108406294+tongari123@users.noreply.github.com>
2022-10-04 21:51:25 +0900

    ユーザー管理機能の実装_修正5

--- db/migrate/20220925084713_devise_create_users.rb
+++ db/migrate/20220925084713_devise_create_users.rb
@@ -11,7 +11,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :family_name,        null: false, default: ""
       t.string :first_name_kana,    null: false, default: ""
       t.string :family_name_kana,   null: false, default: ""
-      t.date   :birthday,           null: false, default: ""
+      t.date   :birthday,           null: false
 
 
 
