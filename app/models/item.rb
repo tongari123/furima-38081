44d57752fada09076ecf45179fe2a3d54246c167
tongari123 <108406294+tongari123@users.noreply.github.com>
2022-10-10 15:45:35 +0900

    出品機能1

---
+++ app/models/item.rb
@@ -0,0 +1,33 @@
+class Item < ApplicationRecord
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :user
+  
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :charge
+  belongs_to :address
+  belongs_to :preparation_day
+
+  with_options presence: true do
+    validates :user_id
+    validates :name
+    validates :introduction
+    validates :category_id
+    validates :item_condition_id
+    validates :charges_id
+    validates :address_id
+    validates :preparation_day_id
+    # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id
+    validates :item_condition_id
+    validates :charges_id
+    validates :address_id
+    validates :preparation_day_id
+  end
+end
