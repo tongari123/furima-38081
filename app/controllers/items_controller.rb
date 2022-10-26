0b709c8bcd651c0a94759c05719cb92bd140d7cc
tongari123 <108406294+tongari123@users.noreply.github.com>
2022-10-26 21:48:30 +0900

    商品削除機能3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,13 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show,:edit,:update,:destroy]
+  before_action :user_signed, only: [:edit,:destroy]
+
 
 
   def index
    @items = Item.includes(:user).order('created_at DESC')
-end
+  end
 
   def new
   @item = Item.new
@@ -24,10 +26,6 @@ end
   end
   
   def edit
-    if @item.user_id == current_user.id
-    else
-      redirect_to root_path
-    end
   end
 
   def update
@@ -39,13 +37,11 @@ end
   end
 
   def destroy
-    if @item.user_id == current_user.id
-      @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
+    if @item.destroy
+    redirect_to root_path
     end
   end
+
 
   private
 
@@ -58,10 +54,8 @@ end
   end
 
   def user_signed
-    if user_signed_in? && current_user.id == @item.user_id
+    unless @item.user_id == current_user.id
       redirect_to root_path
-
     end
   end
-
- end
+end
