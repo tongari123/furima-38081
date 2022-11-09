01f3698d5b08ac481a12f3af2f75b4ad3ffe7f98
tongari123 <108406294+tongari123@users.noreply.github.com>
2022-11-09 20:10:38 +0900

    商品購入機能2

--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -1,5 +1,15 @@
+
+
 FactoryBot.define do
   factory :purchase_form do
-    
+    user_id { Faker::Number.non_zero_digit }
+    item_id { Faker::Number.non_zero_digit }
+    post_code { Faker::Number.decimal_part(digits: 3) + '-' + Faker::Number.decimal_part(digits: 4) }
+    address_id { Faker::Number.between(from: 1, to: 47) }
+    municipality { Faker::Address.city }
+    house_number { Faker::Address.street_address }
+    building_name { Faker::Address.street_address }
+    phone_number { Faker::Number.decimal_part(digits: 11) }
+    token { Faker::Internet.password(min_length: 20, max_length: 30) }
   end
 end
--- spec/models/purchase_form_spec.rb
+++ spec/models/purchase_form_spec.rb
@@ -1,5 +1,107 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseForm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @purchase_form = FactoryBot.build(:purchase_form)
+  end
+
+  describe '配送先情報の保存' do
+    context '配送先情報の保存ができるとき' do
+      it 'すべての値が正しく入力されていれば保存できること' do
+        expect(@purchase_form).to be_valid
+      end
+      it 'user_idが空でなければ保存できる' do
+        @purchase_form.user_id = 1
+        expect(@purchase_form).to be_valid
+      end
+      it 'item_idが空でなければ保存できる' do
+        @purchase_form.item_id = 1
+        expect(@purchase_form).to be_valid
+      end
+      it '郵便番号が「3桁＋ハイフン＋4桁」の組み合わせであれば保存できる' do
+        @purchase_form.post_code = '123-4560'
+        expect(@purchase_form).to be_valid
+      end
+      it '都道府県が「---」以外かつ空でなければ保存できる' do
+        @purchase_form.address_id = 1
+        expect(@purchase_form).to be_valid
+      end
+      it '市区町村が空でなければ保存できる' do
+        @purchase_form.municipality = '横浜市'
+        expect(@purchase_form).to be_valid
+      end
+      it '番地が空でなければ保存できる' do
+        @purchase_form.house_number = '旭区１２３'
+        expect(@purchase_form).to be_valid
+      end
+      it '電話番号が11番桁以内かつハイフンなしであれば保存できる' do
+        @purchase_form.phone_number = 12_345_678_910
+        expect(@purchase_form).to be_valid
+      end
+    end
+
+    context '配送先情報の保存ができないとき' do
+      it 'user_idが空だと保存できない' do
+        @purchase_form.user_id = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空だと保存できない' do
+        @purchase_form.item_id = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Item can't be blank")
+      end
+      it '郵便番号が空だと保存できないこと' do
+        @purchase_form.post_code = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Post code can't be blank", 'Post code is invalid. Include hyphen(-)')
+      end
+      it '郵便番号にハイフンがないと保存できないこと' do
+        @purchase_form.post_code = 1_234_567
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include('Post code is invalid. Include hyphen(-)')
+      end
+      it '都道府県が「---」だと保存できないこと' do
+        @purchase_form.address_id = 0
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Address can't be blank")
+      end
+      it '都道府県が空だと保存できないこと' do
+        @purchase_form.address_id = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Address can't be blank")
+      end
+      it '市区町村が空だと保存できないこと' do
+        @purchase_form.municipality = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Municipality can't be blank")
+      end
+      it '番地が空だと保存できないこと' do
+        @purchase_form.house_number = nil
+        @purchase_form.valid?
+        binding.pry
+        expect(@purchase_form.errors.full_messages).to include("House number can't be blank")
+      end
+      it '電話番号が空だと保存できないこと' do
+        @purchase_form.phone_number = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it '電話番号にハイフンがあると保存できないこと' do
+        @purchase_form.phone_number = '123 - 1234 - 1234'
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include('Phone number is invalid')
+      end
+      it '電話番号が12桁以上あると保存できないこと' do
+        @purchase_form.phone_number = 12_345_678_910_123_111
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include('Phone number is invalid')
+      end
+      it 'トークンが空だと保存できないこと' do
+        @purchase_form.token = nil
+        @purchase_form.valid?
+        expect(@purchase_form.errors.full_messages).to include("Token can't be blank")
+      end
+    end
+  end
 end
